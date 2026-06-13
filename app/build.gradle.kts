e7a404bed33bf699391a205d249b08ad93413099
gusta <txe.dev@gmail.com>
2026-06-13 13:54:47 -0300

    Refatoração de Código: - CameraScreen - ImageScanScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.app.kanjistudy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
